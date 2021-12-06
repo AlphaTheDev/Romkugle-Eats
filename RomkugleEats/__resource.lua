b6dddd876928cf84f3db76b11ae1be4b6f3e9cd5
A1pha <95364000+AlphaTheDev@users.noreply.github.com>
2021-12-06 19:00:41 +0100

    Add files via upload

---
+++ RomkugleEats/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'data/ems/vehicles.meta',
+    'data/ems/carvariations.meta',
+    'data/ems/carcols.meta',
+    'data/ems/handling.meta',
+    'data/job/vehicles.meta',
+    'data/job/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/ems/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/ems/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/ems/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/ems/carvariations.meta'
+data_file 'CARCOLS_FILE' 'data/job/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/job/handling.meta'
